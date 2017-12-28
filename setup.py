613b2d7c31d51cc143de53812ee63996a2fb18c3
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2017-12-28 21:10:01 +0000

    Rewrite setup.py to use setuptools.
    This should make it easier to add dependencies and so on.

--- setup.py
+++ setup.py
@@ -1,27 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-  name = 'glucometerutils',
-  packages = ['glucometerutils', 'glucometerutils.drivers', 'glucometerutils.support'],
-  scripts = ['glucometer.py'],
-  version = '1',
-  description = 'Glucometer access utilities',
-  author = 'Diego Elio Pettenò',
-  author_email = 'flameeyes@flameeyes.eu',
-  url = 'https://www.flameeyes.eu/p/glucometerutils',
-  download_url = 'https://www.flameeyes.eu/files/glucometerutils.tgz',
-  keywords = ['glucometer', 'diabetes'],
-  python_requires = '~=3.4',
-  classifiers = [
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'Operating System :: OS Independent',
-    'License :: OSI Approved :: MIT License',
-    'Intended Audience :: End Users/Desktop',
-    'Topic :: Scientific/Engineering :: Medical Science Apps.',
-  ],
+    name = 'glucometerutils',
+    version = '1',
+    description = 'Glucometer access utilities',
+    author = 'Diego Elio Pettenò',
+    author_email = 'flameeyes@flameeyes.eu',
+    url = 'https://www.flameeyes.eu/p/glucometerutils',
+    download_url = 'https://www.flameeyes.eu/files/glucometerutils.tgz',
+    keywords = ['glucometer', 'diabetes'],
+    python_requires = '~=3.4',
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    ],
+    packages = find_packages(
+        exclude=['test', 'udev']),
+    data_files = [
+        ('lib/udev/rules', ['udev/69-glucometerutils.rules']),
+    ],
+    extras_require = {
+        'test': ['abseil-py'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'glucometer=glucometerutils.glucometer:main'
+        ]
+    },
 )
