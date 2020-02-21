64bc2f83ba8285c0a3428801f03e99abe70a3f0c
Ben <b-schaefer@posteo.de>
2020-02-21 10:12:34 +0100

    add configuration for pre-commit, black and isort

--- setup.py
+++ setup.py
@@ -4,74 +4,69 @@
 
 import sys
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 from setuptools.command.test import test as TestCommand
 
-
-with open('test-requirements.txt') as requirements:
-    test_required = requirements.read().splitlines()
+test_required = [
+    "absl-py",
+    "construct>=2.9",
+    "pytest>=3.6.0",
+    "pytest-timeout>=1.3.0",
+    "pyserial",
+]
 
 
 class PyTestCommand(TestCommand):
-
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main([])
         sys.exit(errno)
 
 
 setup(
-    name = 'glucometerutils',
-    version = '1',
-    description = 'Glucometer access utilities',
-    author = 'Diego Elio Pettenò',
-    author_email = 'flameeyes@flameeyes.com',
-    url = 'https://www.flameeyes.com/p/glucometerutils',
-    download_url = 'https://www.flameeyes.com/files/glucometerutils.tgz',
-    keywords = ['glucometer', 'diabetes'],
-    python_requires = '~=3.5',
-    classifiers = [
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: End Users/Desktop',
-        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    name="glucometerutils",
+    version="1",
+    description="Glucometer access utilities",
+    author="Diego Elio Pettenò",
+    author_email="flameeyes@flameeyes.com",
+    url="https://www.flameeyes.com/p/glucometerutils",
+    download_url="https://www.flameeyes.com/files/glucometerutils.tgz",
+    keywords=["glucometer", "diabetes"],
+    python_requires="~=3.5",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
     ],
-    packages = find_packages(
-        exclude=['test', 'udev']),
-    data_files = [
-        ('lib/udev/rules', ['udev/69-glucometerutils.rules']),
-    ],
-    install_requires = [
-        'attrs',
-    ],
-    tests_require = test_required,
-    extras_require = {
+    packages=find_packages(exclude=["test", "udev"]),
+    data_files=[("lib/udev/rules", ["udev/69-glucometerutils.rules"]),],
+    install_requires=["attrs",],
+    tests_require=test_required,
+    extras_require={
         # These are all the drivers' dependencies. Optional dependencies are
         # listed as mandatory for the feature.
-        'accucheck_reports': [],
-        'contourusb': ['construct', 'hidapi'],
-        'fsinsulinx': ['construct', 'hidapi'],
-        'fslibre': ['construct', 'hidapi'],
-        'fsoptium': ['pyserial'],
-        'fsprecisionneo': ['construct', 'hidapi'],
-        'otultra2': ['pyserial'],
-        'otultraeasy': ['construct', 'pyserial'],
-        'otverio2015': ['construct', 'python-scsi'],
-        'otverioiq': ['construct', 'pyserial'],
-        'sdcodefree': ['construct', 'pyserial'],
-        'td4277': ['construct', 'pyserial', 'hidapi'],
+        "accucheck_reports": [],
+        "contourusb": ["construct", "hidapi"],
+        "fsinsulinx": ["construct", "hidapi"],
+        "fslibre": ["construct", "hidapi"],
+        "fsoptium": ["pyserial"],
+        "fsprecisionneo": ["construct", "hidapi"],
+        "otultra2": ["pyserial"],
+        "otultraeasy": ["construct", "pyserial"],
+        "otverio2015": ["construct", "python-scsi"],
+        "otverioiq": ["construct", "pyserial"],
+        "sdcodefree": ["construct", "pyserial"],
+        "td4277": ["construct", "pyserial", "hidapi"],
+        # Development and testing dependencies
+        "dev": test_required + ["pre-commit"],
     },
-    entry_points = {
-        'console_scripts': [
-            'glucometer=glucometerutils.glucometer:main'
-        ]
-    },
-    cmdclass = {
-        'test': PyTestCommand,
-    },
+    entry_points={"console_scripts": ["glucometer=glucometerutils.glucometer:main"]},
+    cmdclass={"test": PyTestCommand,},
 )
