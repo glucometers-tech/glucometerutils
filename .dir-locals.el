887d78b91be2fb16519187a000e17e50c16686f3
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-03-14 17:48:27 +0000

    Expand line length in Emacs to match black.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ; SPDX-License-Identifier: MIT
-((nil . ((fill-column . 80)))
+((nil . ((fill-column . 88)))
  (markdown-mode . ((mode . flyspell)))
  (python-mode . ((mode . flyspell-prog)
 		 (mode . whitespace))))
