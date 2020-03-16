f5143327b243fc616f37252d76bd31f2690b088d
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-03-16 18:51:27 +0000

    Move tests to be organized within the source directory.
    This should simplify maintaining the tests in the long run, and allows mypy
    to more thoroughly check for types.

--- test/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# SPDX-License-Identifier: MIT
-"""Add the top-level module to the PYTHONPATH."""
-
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
