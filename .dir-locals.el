40818e0d0a4d33c15c94aba4351efbeaa679e01f
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-03-26 18:46:51 +0000

    Update the local Emacs config to match the black/isort/flake8 configs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ; SPDX-License-Identifier: MIT
-((nil . ((fill-column . 88)))
+((nil . ((fill-column . 88)
+	 (whitespace-line-column . 88)))
  (markdown-mode . ((mode . flyspell)))
  (python-mode . ((mode . flyspell-prog)
 		 (mode . whitespace))))
